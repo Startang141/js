7c6c26d463a27f2cfb396df5e31920406824e791
startang141m <bintangadiyatma141@gmail.com>
2023-03-15 23:02:28 +0700

    ES6-Map, Filter, ForEach

--- ES6/main.js
+++ ES6/main.js
@@ -10,16 +10,15 @@
 //Object Literals
 // //ambil element
 // const cart_items = document.getElementById('cart')
-
 // function addCart(name, category) {
 //     return {
 //         name: name,
 //         category: category
 //     }
 // }
-
 // var getProcudt = addCart("iphone 14", "gadget");
 // cart_items.innerHTML = "<b>Product : </b> " + getProcudt.name + " <b>Kategori : </b> " + getProcudt.category;
+
 // ES6
 
 // String Concatenation
@@ -29,16 +28,38 @@
 // const user_logged = "tony";
 // user_place.innerHTML = `Hi, ${user_logged} asik`;
 
-//Object Literals
-//ambil element
-const cart_items = document.getElementById('cart')
+// //Object Literals
+// //ambil element
+// const cart_items = document.getElementById('cart')
+// function addCart(name, category) {
+//     return {
+//         name,
+//         category
+//     }
+// }
+// var getProcudt = addCart("iphone 14", "gadget");
+// cart_items.innerHTML = `<b>Product : </b>  ${getProcudt.name} <b> Kategori: </b>  ${getProcudt.category}`;
 
-function addCart(name, category) {
-    return {
-        name,
-        category
-    }
-}
+// // Array ForEach 
+// Get Element 
+// const skills_holder = document.getElementById('skills')
+// const yourSkills = ['UX Design', 'Web Developer', 'Video Editor'];
+// let parent = '<ul>';
+// yourSkills.forEach((skill) => {
+//     parent += '<li>' + skill + '</li>';
+// });
 
-var getProcudt = addCart("iphone 14", "gadget");
-cart_items.innerHTML = `<b>Product : </b>  ${getProcudt.name} <b> Kategori: </b>  ${getProcudt.category}`;
+// parent += '</ul>';
+// skills_holder.innerHTML = parent;
+
+//map
+// const printSkill = yourSkills.map((skill) => {
+//     return skill;
+// });
+// skills_holder.innerHTML = printSkill;
+
+// filter
+// const myPrimarySkill = yourSkills.filter((skill) => {
+//     return skill === 'UX Design';
+// });
+// skills_holder.innerHTML = myPrimarySkill;
