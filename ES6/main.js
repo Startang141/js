37487906a3fbd81ec04565330070ec363d5c6466
startang141m <bintangadiyatma141@gmail.com>
2023-03-15 21:46:16 +0700

    ES6 - Object Literals

--- ES6/main.js
+++ ES6/main.js
@@ -1,15 +1,44 @@
 // ES5
-// String Concatenation
-// Ambil Element
-const user_place = document.getElementById('user');
-//seolah-olah login
-const user_logged = "tony";
-user_place.innerHTML = "Hi," + user_logged + " asik";
 
-// ES6
 // String Concatenation
-// Ambil Element
-const user_place = document.getElementById('user');
-//seolah-olah login
-const user_logged = "tony";
-user_place.innerHTML = `Hi, ${user_logged} asik`;
+// // Ambil Element
+// const user_place = document.getElementById('user');
+// //seolah-olah login
+// const user_logged = "tony";
+// user_place.innerHTML = "Hi," + user_logged + " asik";
+
+//Object Literals
+// //ambil element
+// const cart_items = document.getElementById('cart')
+
+// function addCart(name, category) {
+//     return {
+//         name: name,
+//         category: category
+//     }
+// }
+
+// var getProcudt = addCart("iphone 14", "gadget");
+// cart_items.innerHTML = "<b>Product : </b> " + getProcudt.name + " <b>Kategori : </b> " + getProcudt.category;
+// ES6
+
+// String Concatenation
+// // Ambil Element
+// const user_place = document.getElementById('user');
+// //seolah-olah login
+// const user_logged = "tony";
+// user_place.innerHTML = `Hi, ${user_logged} asik`;
+
+//Object Literals
+//ambil element
+const cart_items = document.getElementById('cart')
+
+function addCart(name, category) {
+    return {
+        name,
+        category
+    }
+}
+
+var getProcudt = addCart("iphone 14", "gadget");
+cart_items.innerHTML = `<b>Product : </b>  ${getProcudt.name} <b> Kategori: </b>  ${getProcudt.category}`;
